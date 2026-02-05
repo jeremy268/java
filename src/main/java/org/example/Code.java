8b18e7861e719d9dcfea319a4068026f3ea5546a
Jeremy <nyebejerry2009123@gmail.com>
2026-02-05 12:28:13 +0100

    1st commit

---
+++ src/main/java/org/example/Code.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Code {
+    public static void main(String[] args) {
+        int studentID = 56;
+        double score = 7.88;
+        char grade = 'Q';
+        System.out.println(studentID);
+        System.out.println(score);
+        System.out.println(grade);
+    }
+}
