970832167bb624a81aa6cff496e3d3d02a1fabe5
Jeremy <nyebejerry2009123@gmail.com>
2026-02-09 11:10:38 +0100

    first commit

---
+++ src/main/java/CodeChallenge.java
@@ -0,0 +1,8 @@
+public class CodeChallenge {
+    public static void main(String[] args) {
+        int itemPrice = 8;
+        int shippingCost = 9;
+        int sum  = itemPrice + shippingCost;
+        System.out.println(sum);
+    }
+}
