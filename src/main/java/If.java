4a79bfc9deeaafc000930ba7c4a93f6752fc699b
Jeremy <nyebejerry2009123@gmail.com>
2026-02-18 11:13:38 +0100

    second commit

---
+++ src/main/java/If.java
@@ -0,0 +1,12 @@
+public class If {
+    public static void main(String[] args){
+        int age = 20;
+
+        if (age >=18){
+            System.out.println("Allowed");
+        }
+        else {
+            System.out.println("Not allowed");
+        }
+    }
+}
---
+++ src/main/java/Just.java
@@ -0,0 +1,7 @@
+public class Just {
+    public static void main(String[] args){
+    String message = "Good morning";
+        System.out.println(message);
+    }
+}
+
